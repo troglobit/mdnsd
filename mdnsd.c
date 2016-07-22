8ce1e1c676801d4170513f28734cca5b6a0333c8
Stefan Profanter <git@s.profanter.me>
2016-07-22 11:10:42 +0200

    Auto republish if TTL nearly expired

--- mdnsd.c
+++ mdnsd.c
@@ -104,7 +104,7 @@ int msock(void)
 	int s, flag = 1, ittl = 255;
 	struct sockaddr_in in;
 	struct ip_mreq mc;
-	char ttl = 255;
+	char ttl = 255; // send to any reachable net, not only the subnet
 
 	memset(&in, 0, sizeof(in));
 	in.sin_family = AF_INET;
