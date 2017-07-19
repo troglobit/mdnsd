troglobit/mdnsd

commit 7fca9c3b553f90bc749d027a67903396f815bcc6

Fix clang compiler warnings in strict mode

libmdnsd/1035.c
@@ -308,11 +308,12 @@ static int _rrparse(struct message *m, struct resource *rr, int count, unsigned 
 }
 
 /* Keep all our mem in one (aligned) block for easy freeing */
-#define my(x,y, cast)					\
+#define my(x,y, cast)				\
 	while (m->_len & 7)			\
 		m->_len++;			\
-	x = (cast)(m->_packet + m->_len);	\
-	m->_len += y;
+		                        \
+	memcpy(x, (m->_packet + m->_len), sizeof(cast)); \
+	m->_len += y;         
 
 void message_parse(struct message *m, unsigned char *packet)
 {
@@ -365,7 +366,7 @@ void message_parse(struct message *m, unsigned char *packet)
 	}
 
 	/* Process questions */
-	my(m->qd, (int)(sizeof(struct question) * m->qdcount), struct question *);
+	my(m->qd, (int)(sizeof(struct question) * m->qdcount), struct question);
 	for (i = 0; i < m->qdcount; i++) {
 		_label(m, &buf, &(m->qd[i].name));
 		m->qd[i].type  = net2short(&buf);
@@ -373,9 +374,9 @@ void message_parse(struct message *m, unsigned char *packet)
 	}
 
 	/* Process rrs */
-	my(m->an, (int)(sizeof(struct resource) * m->ancount), struct resource *);
-	my(m->ns, (int)(sizeof(struct resource) * m->nscount), struct resource *);
-	my(m->ar, (int)(sizeof(struct resource) * m->arcount), struct resource *);
+	my(m->an, (int)(sizeof(struct resource) * m->ancount), struct resource);
+	my(m->ns, (int)(sizeof(struct resource) * m->nscount), struct resource);
+	my(m->ar, (int)(sizeof(struct resource) * m->arcount), struct resource);
 	if (_rrparse(m, m->an, m->ancount, &buf))
 		return;
 	if (_rrparse(m, m->ns, m->nscount, &buf))

libmdnsd/mdnsd.c
@@ -1223,8 +1223,14 @@ unsigned short int mdnsd_step(mdns_daemon_t *d, int mdns_socket, bool processIn,
 		struct sockaddr_in to;
 		struct in_addr ip;
 		unsigned short int port;
-
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-align"
+#endif
 		while (mdnsd_out(d, &m, (long unsigned int *)&ip, &port)) {
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif	
 			int len = message_packet_len(&m);
 			char* buf = (char*)message_packet(&m);
 			memset(&to, 0, sizeof(to));

commit b72f2c573add9aa6e58cd019f7e5c5c9b84cb585

Fixup for commit 7fca9c3b553f90bc749d027a67903396f815bcc6 which introduced a segfault

libmdnsd/1035.c
@@ -312,8 +312,8 @@ static int _rrparse(struct message *m, struct resource *rr, int count, unsigned 
 	while (m->_len & 7)			\
 		m->_len++;			\
 		                        \
-	memcpy(x, (m->_packet + m->_len), sizeof(cast)); \
-	m->_len += y;         
+	x = (cast)(void *)(m->_packet + m->_len);	\
+	m->_len += y;
 
 void message_parse(struct message *m, unsigned char *packet)
 {
@@ -366,7 +366,7 @@ void message_parse(struct message *m, unsigned char *packet)
 	}
 
 	/* Process questions */
-	my(m->qd, (int)(sizeof(struct question) * m->qdcount), struct question);
+	my(m->qd, (int)(sizeof(struct question) * m->qdcount), struct question *);
 	for (i = 0; i < m->qdcount; i++) {
 		_label(m, &buf, &(m->qd[i].name));
 		m->qd[i].type  = net2short(&buf);
@@ -374,9 +374,9 @@ void message_parse(struct message *m, unsigned char *packet)
 	}
 
 	/* Process rrs */
-	my(m->an, (int)(sizeof(struct resource) * m->ancount), struct resource);
-	my(m->ns, (int)(sizeof(struct resource) * m->nscount), struct resource);
-	my(m->ar, (int)(sizeof(struct resource) * m->arcount), struct resource);
+	my(m->an, (int)(sizeof(struct resource) * m->ancount), struct resource *);
+	my(m->ns, (int)(sizeof(struct resource) * m->nscount), struct resource *);
+	my(m->ar, (int)(sizeof(struct resource) * m->arcount), struct resource *);
 	if (_rrparse(m, m->an, m->ancount, &buf))
 		return;
 	if (_rrparse(m, m->ns, m->nscount, &buf))

mquery.c
@@ -69,7 +69,7 @@ int msock(void)
 	int s, flag = 1, ittl = 255;
 	struct sockaddr_in in;
 	struct ip_mreq mc;
-	char ttl = 255;
+	char ttl = (char) 255;
 
 	memset(&in, 0, sizeof(in));
 	in.sin_family = AF_INET;