troglobit/mdnsd

commit 8e2d83a944a94d63415ad8403d34760a18317bf9

Update build matrix to include gcc7

libmdnsd/1035.c
@@ -205,7 +205,8 @@ static int _host(struct message *m, unsigned char **bufp, char *name)
 			if (_lmatch(m, label + x, m->_labels[y])) {
 				/* Matching label, set up pointer */
 				l = label + x;
-				short2net((unsigned short)((unsigned char *)m->_labels[y] - m->_packet), (unsigned char **)&l);
+				unsigned char* target = (unsigned char *) &(l);
+				short2net((unsigned short)((unsigned char *)m->_labels[y] - m->_packet), &target);
 				label[x] = (char)(label[x] | 0xc0);
 				len = x + 2;
 				break;

.travis.yml
@@ -1,47 +1,120 @@
-language: c
-
-compiler:
-- gcc
-- clang
-
-os:
-- linux
-- osx
+# using c for language overwrites our compilers
+language: generic
 
 dist: trusty
-sudo: required
-
-env:
-  matrix:
-    # Do both, compile with static code analysis and without
-    - ANALYZE=false
-    - ANALYZE=true
+sudo: false
 
 matrix:
-  exclude:
-  # This excludes OSX GCC builds.
+  include:
+    - os: linux
+      addons:
+        apt:
+          sources:
+            - ubuntu-toolchain-r-test
+          packages:
+            - binutils-mingw-w64-i686
+            - build-essential
+            - gcc-multilib
+            - gcc-4.9
+            - gcc-mingw-w64-i686
+            - g++-4.9
+            - g++-multilib
+            - g++-mingw-w64
+            - libc6-dev-i386
+            - linux-libc-dev:i386
+            - mingw-w64
+      env:
+         - ANALYZE=false
+         - MINGW=true
+         - CC=gcc-4.9
+         - CXX=g++-4.9
+    - os: linux
+      addons:
+        apt:
+          sources:
+            - ubuntu-toolchain-r-test
+          packages:
+            - gcc-7
+            - gcc-7-multilib
+            - g++-7
+            - g++-7-multilib
+      env:
+         - ANALYZE=false
+         - CC=gcc-7
+         - CXX=g++-7
+    - os: linux
+      addons:
+        apt:
+          sources:
+            - llvm-toolchain-trusty-6.0
+            - ubuntu-toolchain-r-test
+          packages:
+            - clang-3.9
+      env:
+         - ANALYZE=false
+         - CC=clang-3.9
+         - CXX=clang++-3.9
+    - os: linux
+      addons:
+        apt:
+          sources:
+            - llvm-toolchain-trusty-6.0
+            - ubuntu-toolchain-r-test
+          packages:
+            - clang-6.0
+            - clang-tidy-6.0
+      env:
+         - ANALYZE=false
+         - CC=clang-6.0
+         - CXX=clang++-6.0
+    - os: linux
+      addons:
+        apt:
+          sources:
+            - ubuntu-toolchain-r-test
+          packages:
+            - gcc-7
+            - gcc-7-multilib
+            - g++-7
+            - g++-7-multilib
+            - linux-libc-dev:i386
+      env:
+         - ANALYZE=true
+         - CC=gcc-7
+         - CXX=g++-7
+    - os: linux
+      addons:
+        apt:
+          sources:
+            - llvm-toolchain-trusty-6.0
+            - ubuntu-toolchain-r-test
+          packages:
+            - clang-6.0
+            - clang-tidy-6.0
+      env:
+         - ANALYZE=true
+         - CC=clang-6.0
+         - CXX=clang++-6.0
     - os: osx
-      compiler: gcc
-    - os: osx
-      env: ANALYZE=true
+      compiler: clang
+      env:
+         - ANALYZE=false
 
-addons:
-  apt:
-    sources:
-      # see https://github.com/travis-ci/apt-source-whitelist/blob/master/ubuntu.json
-    packages:
-      - binutils-mingw-w64-i686
-      - build-essential
-      - cmake
-      - gcc-mingw-w64-i686
-      - gcc-multilib
-      - g++-multilib
-      - gcc-4.8
-      - g++-4.8
-      - g++-4.8-multilib
-      - libc6-dbg # for valgrind compilation
-      - mingw-w64
-      - g++-mingw-w64
+#addons:
+#  apt:
+#    sources:
+#      # see https://github.com/travis-ci/apt-source-whitelist/blob/master/ubuntu.json
+#    packages:
+#      - binutils-mingw-w64-i686
+#      - build-essential
+#      - cmake
+#      - mingw32-gcc
+#      - gcc-mingw-w64-i686
+#      - gcc-mingw-w64
+#      - g++-mingw-w64
+#      - gcc-multilib
+#      - g++-multilib
+#      - libc6-dbg # for valgrind compilation
 
 cache:
   pip: true

libmdnsd/CMakeLists.txt
@@ -10,7 +10,7 @@ mark_as_advanced(MDNSD_COMPILE_AS_CXX)
 
 if(NOT MDNSD_COMPILE_AS_CXX AND (CMAKE_COMPILER_IS_GNUCC OR "x${CMAKE_C_COMPILER_ID}" STREQUAL "xClang"))
   add_definitions(-std=c99 -pipe
-                  -Wall -Wextra -Werror -Wpedantic
+                  -Wall -Wextra -Werror
                   -Wno-unused-parameter # some methods may require unused arguments to cast to a method pointer
                   -Wmissing-prototypes -Wstrict-prototypes -Wredundant-decls
                   -Wformat -Wformat-security -Wformat-nonliteral
@@ -21,6 +21,13 @@ if(NOT MDNSD_COMPILE_AS_CXX AND (CMAKE_COMPILER_IS_GNUCC OR "x${CMAKE_C_COMPILER
                   -Wmultichar
                   -Wundef
                   -Wc++-compat)
+  if(NOT MINGW AND NOT MSYS AND NOT MSVC)
+    add_definitions(-Wpedantic)
+  endif()
+  if(UNIX AND NOT APPLE AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 7.0)
+    # new feature since 7.0
+    add_definitions(-Wformat-truncation=1)
+  endif()
   if(NOT WIN32 AND NOT CYGWIN)
     add_definitions(-Wshadow -Wconversion -fvisibility=hidden -fPIC)
   endif()

tools/travis/travis_linux_before_install.sh
@@ -3,12 +3,12 @@ set -ev
 
 echo "=== Updating the build environment in $LOCAL_PKG ==="
 
-echo "=== Installing from external package sources ==="
-wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main" | sudo tee -a /etc/apt/sources.list
-sudo add-apt-repository -y ppa:lttng/ppa
-sudo apt-get update -qq
-sudo apt-get install -y clang-3.9 clang-tidy-3.9
+#echo "=== Installing from external package sources ==="
+#wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+#echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main" | sudo tee -a /etc/apt/sources.list
+#sudo add-apt-repository -y ppa:lttng/ppa
+#sudo apt-get update -qq
+#sudo apt-get install -y clang-3.9 clang-tidy-3.9
 
 echo "=== Installing python packages ==="
 pip install --user cpp-coveralls
@@ -43,7 +43,7 @@ echo "=== Installing valgrind ==="
 wget ftp://sourceware.org/pub/valgrind/valgrind-3.13.0.tar.bz2
 tar xf valgrind-3.13.0.tar.bz2
 cd valgrind-3.13.0
-./configure --prefix=$LOCAL_PKG
+CC=clang CXX=clang++ ./configure --prefix=$LOCAL_PKG
 make -s -j8 install
 cd ..
 
@@ -66,8 +66,8 @@ echo "=== Installing cppcheck ==="
 wget https://github.com/danmar/cppcheck/archive/1.73.tar.gz -O cppcheck-1.73.tar.gz
 tar xf cppcheck-1.73.tar.gz
 cd cppcheck-1.73
-make PREFIX="$LOCAL_PKG" SRCDIR=build CFGDIR="$LOCAL_PKG/cppcheck-cfg" HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" -j8
-make PREFIX="$LOCAL_PKG" SRCDIR=build CFGDIR="$LOCAL_PKG/cppcheck-cfg" HAVE_RULES=yes install
+CC=clang CXX=clang++ make PREFIX="$LOCAL_PKG" SRCDIR=build CFGDIR="$LOCAL_PKG/cppcheck-cfg" HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wno-sign-compare -Wno-unused-function -std=gnu++0x" -j
+CC=clang CXX=clang++ make PREFIX="$LOCAL_PKG" SRCDIR=build CFGDIR="$LOCAL_PKG/cppcheck-cfg" HAVE_RULES=yes install
 cd ..
 
 # create cached flag

tools/travis/travis_linux_script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -ev
 
+echo "\n\n---------------------------------------------------\n###### Running with $CC and Analyze=$ANALYZE ######\n---------------------------------------------------\n\n"
+
 if [ $ANALYZE = "true" ]; then
     echo "=== Running static code analysis ==="
-    if [ "$CC" = "clang" ]; then
+    if ! case $CC in clang*) false;; esac; then
         mkdir -p build
         cd build
-        scan-build-3.9 cmake -G "Unix Makefiles" ..
-        scan-build-3.9 -enable-checker security.FloatLoopCounter \
+        scan-build cmake -G "Unix Makefiles" ..
+        scan-build -enable-checker security.FloatLoopCounter \
           -enable-checker security.insecureAPI.UncheckedReturn \
           --status-bugs -v \
           make -j
@@ -27,8 +29,7 @@ if [ $ANALYZE = "true" ]; then
 else
     echo "=== Building ==="
 
-    # cross compilation only with gcc
-    if [ "$CC" = "gcc" ]; then
+    if [ $MINGW = "true" ]; then
         echo "Cross compile release build for MinGW 32 bit"
         mkdir -p build && cd build
         cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-mingw32.cmake -DCMAKE_BUILD_TYPE=Release ..
@@ -46,11 +47,6 @@ else
         cmake -DCMAKE_TOOLCHAIN_FILE=../tools/cmake/Toolchain-gcc-m32.cmake -DCMAKE_BUILD_TYPE=Release ..
         make -j
         cd .. && rm build -rf
-    fi
-
-    if [ "$CC" = "gcc" ]; then
-        echo "Upgrade to gcc 4.8"
-        export CXX="g++-4.8" CC="gcc-4.8"
     fi
 
     echo "Compile release build for 64-bit linux"

commit e98eab03631bdf57f39b3cb9acce1ee1786aa411

Fixup for bug introduced in 8e2d83a944a94d63415ad8403d34760a18317bf9

libmdnsd/1035.c
@@ -217,8 +217,7 @@ static int _host(struct message *m, unsigned char **bufp, char *name)
 			if (_lmatch(m, label + x, m->_labels[y])) {
 				/* Matching label, set up pointer */
 				l = label + x;
-				unsigned char* target = (unsigned char *) &(l);
-				short2net((unsigned short)((unsigned char *)m->_labels[y] - m->_packet), &target);
+				short2net((unsigned short)((unsigned char *)m->_labels[y] - m->_packet), (unsigned char **)&l);
 				label[x] = (char)(label[x] | 0xc0);
 				len = x + 2;
 				break;