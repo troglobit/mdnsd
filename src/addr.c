494e23e739061f9323079d8992d1737db01da078
Joachim Wiberg <troglobit@gmail.com>
2026-06-25 18:37:46 +0200

    Bump copyright years to 2026
    Third-party headers keep their own years: lib/strlcpy.c (Todd C. Miller)
    and src/mcsock.[ch] (Florian Zschocke).
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/addr.c
+++ src/addr.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2022  Joachim Wiberg <troglobit@gmail.com>
+ * Copyright (c) 2018-2026  Joachim Wiberg <troglobit@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
